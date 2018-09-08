73faa5a9acb2c3d7adb99eeda9e95e9df9de4d54
Yurie <cutecupcakeandcute@gmail.com>
2018-09-08 20:57:13 +0900

    edit

--- starter2nd/app.js
+++ starter2nd/app.js
@@ -8,3 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+//setter
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//getter
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    //when event('click')happens, function will be called back
+    //1. random number will be set to dice, and change dice img accordingly.
+    dice = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('.dice').style.display = 'block';
+    document.querySelector('.dice').src = 'dice-' + dice + '.png';
+    //2. the number of dice will be displayed on current score, and will add up until next player
+    //3. if number of dice is 1, change to next player
+    if (dice !== 1) {
+        roundScore = roundScore + dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        //notify that user got 1
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        //skip to next player;
+
+    }
+})
